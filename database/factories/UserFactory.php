e4a8f648ca17e541ed3ece9b7c4eb33b52063563
SirGuilhermeBR <sir_guilherme@hotmail.com>
2025-08-24 03:43:32 -0300

    fix: Atualiza UserFactory com dados únicos e timestamps aleatórios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => fake()->password(),
+            'created_at' => fake()->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => fake()->dateTimeBetween('now', 'now'),
         ];
     }
 
