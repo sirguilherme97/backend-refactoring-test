36e79e121c3ca5f139ded57da540592a260ced22
SirGuilhermeBR <sir_guilherme@hotmail.com>
2025-08-24 03:44:29 -0300

    fix: ajuste para usar seeds se necessário

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -11,10 +11,6 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
-            'name' => 'example',
-            'email' => 'example@elevensoft.dev',
-            'password' => bcrypt('password')
-        ]);
+        \App\Models\User::factory(10)->create();
     }
 }
